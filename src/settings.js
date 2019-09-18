ce9ae1c1786fa7849752d00ee9c06cb5c8bc3319
netsun <303078098@qq.com>
2019-09-18 15:49:20 +0800

    fix：修改网页默认标题

--- src/settings.js
+++ src/settings.js
@@ -1,9 +1,9 @@
 module.exports = {
 
-  title: 'Vue Template',
+  title: 'Vue Member Template',
 
-  siteName: 'siteName',
-  siteTitle: 'Vue Template',
+  siteName: 'Vue Website',
+  siteTitle: 'Vue Member Template',
   siteLogo: 'https://wpimg.wallstcn.com/69a1c46c-eb1c-4b46-8bd4-e9e686ef5251.png'
 
 }
